4055667a8501d5f08aa4fcb7452e7abfe18cebd5
delia <deliarata28@yahoo.com>
2026-09-08 13:26:50 +0300

    chore: update ignore files

---
+++ frontend/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      routesDirectory: "./src/app/routes",
+      generatedRouteTree: "./src/routeTree.gen.ts",
+    }),
+    react(),
+  ],
+});
